260322e9d9504da636679984c27b9a5fddca310e
FelipeZelent <felipezelent@gmail.com>
2024-11-19 16:17:47 -0300

    ajustes

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,23 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 db = SQLAlchemy()
 
 def create_app():
-    app = Flask(__name__, instance_relative_config=True)
-    app.config['SECRET_KEY'] = 'dev'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+    app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
+        'DATABASE_URL',
+        'mssql+pyodbc://adminflask:Admin@123456@sqlserver-rm1142282365.database.windows.net:1433/VisionWebApp?driver=ODBC+Driver+18+for+SQL+Server&Encrypt=yes&TrustServerCertificate=no'
+    )
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
 
-    from .routes import configure_routes
-    configure_routes(app)
+    @app.route('/')
+    def index():
+        return "Conexão com o banco de dados configurada!"
 
     with app.app_context():
         db.create_all()
